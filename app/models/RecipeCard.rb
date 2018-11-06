d688fc60be84090077d933afa2ee8ac0cb2cebf2
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-06 06:56:44 -0500

    fill out RecipeCard initialization use reader class attr for getters

--- app/models/RecipeCard.rb
+++ app/models/RecipeCard.rb
@@ -4,32 +4,19 @@
 class RecipeCard
   @@recipe_cards = []
 
-  def initialize
+  attr_reader :date, :rating, :user, :recipe
+
+  def initialize(recipe, user, date, rating)
     @@recipe_cards << self
+
+    @recipe = recipe
+    @user = user
+    @date = date
+    @rating = rating
   end
 
   # return all of the RecipeCard instances
   def self.all
     return @@recipe_cards
-  end
-
-  # return the date of the entry
-  def date
-    return @date
-  end
-
-  # return the rating (an integer) a user has given their entry
-  def rating
-    return @rating
-  end
-
-  # return the user to which the entry belongs
-  def user
-    return @user
-  end
-
-  # return the recipe to which the entry belongs
-  def recipe
-    return @recipe
   end
 end
