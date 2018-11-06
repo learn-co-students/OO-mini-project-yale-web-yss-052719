83cbcc22b9db98eec4e868f833c7861ac9993db6
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-05 23:50:04 -0500

    adding Recipe class instantiation and all

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,14 +1,39 @@
+class Recipe
+  @@recipes = []
 
-# Recipe.all should return all of the recipe instances
+  def initialize
+    @@recipes << self
+  end
 
-# Recipe.most_popular should return the recipe instance with the highest number of users (the recipe that has the most
-# recipe cards)
+  # Recipe.all should return all of the recipe instances
+  def self.all
+    return @@recipes
+  end
 
-# Recipe#users should return the user instances who have recipe cards with this recipe
+  # Recipe.most_popular should return the recipe instance with the highest number of users (the recipe that has the most
+  # recipe cards)
+  def self.most_popular
 
-# Recipe#ingredients should return all of the ingredients in this recipe
+  end
 
-# Recipe#allergens should return all of the ingredients in this recipe that are allergens
+  # Recipe#users should return the user instances who have recipe cards with this recipe
+  def users
 
-# Recipe#add_ingredients should take an array of ingredient instances as an argument, and associate each of those
-# ingredients with this recipe
+  end
+
+  # Recipe#ingredients should return all of the ingredients in this recipe
+  def ingrediants
+
+  end
+
+  # Recipe#allergens should return all of the ingredients in this recipe that are allergens
+  def allergens
+
+  end
+
+  # Recipe#add_ingredients should take an array of ingredient instances as an argument, and associate each of those
+  # ingredients with this recipe
+  def add_ingredients(ingredients)
+
+  end
+end
