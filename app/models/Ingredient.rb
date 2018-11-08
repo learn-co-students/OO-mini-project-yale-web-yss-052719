4ea0ace3fe417cb2e03d3de0b4b0a2b981cf50e0
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-08 13:31:27 -0500

    Ingredient.most_common_allergen: add solution by Brit and collapse a little on my own

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -15,12 +15,17 @@ class Ingredient
     @@ingredients
   end
 
+  # -- Brit solution --
+  # def self.most_common_allergen
+  #   Allergen.all.inject(Hash.new(0)) do |counts, item|
+  #     counts[item.ingredient] += 1
+  #   end.max_by {|key, val| val }.first
+  # end
+
   # return the ingredient instance that the highest number of users are allergic to
   def self.most_common_allergen
     @@ingredients.max_by do |ingredient|
-      Allergen.all.select do |allergen|
-        allergen.ingredient == ingredient
-      end.count
+      Allergen.all.select { |allergen| allergen.ingredient == ingredient }.count
     end
   end
 end
