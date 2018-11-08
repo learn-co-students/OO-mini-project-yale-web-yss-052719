f071075a2d3d9bc106b71b3ebe55f9a024142cb0
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-08 11:54:32 -0500

    implement Ingredient.most_common_allergen

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -17,6 +17,10 @@ class Ingredient
 
   # return the ingredient instance that the highest number of users are allergic to
   def self.most_common_allergen
-
+    @@ingredients.max_by do |ingredient|
+      Allergen.all.select do |allergen|
+        allergen.ingredient == ingredient
+      end.count
+    end
   end
 end
