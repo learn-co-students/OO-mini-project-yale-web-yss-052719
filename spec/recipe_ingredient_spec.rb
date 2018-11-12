6afc58a0e666ae4a60b9ac077c08623f4fa3994b
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-12 11:03:53 -0500

    move test class objects to instance namespace for accessibility in test methods change string literals to be single quote (rubocop suggestion) add test cases for reading/writing class attrs

--- spec/recipe_ingredient_spec.rb
+++ spec/recipe_ingredient_spec.rb
@@ -6,24 +6,46 @@ require 'Recipe.rb'
 
 describe RecipeIngredient do
   before do
-    flour = Ingredient.new("Flour")
-    pepper = Ingredient.new("Pepper")
-    carrot = Ingredient.new("Carrot")
+    @flour = Ingredient.new('Flour')
+    @pepper = Ingredient.new('Pepper')
+    @carrot = Ingredient.new('Carrot')
 
-    pancake = Recipe.new("Pancake")
-    omelette = Recipe.new("Omelette")
-    salad = Recipe.new("Salad")
+    @pancake = Recipe.new('Pancake')
+    @omelette = Recipe.new('Omelette')
+    @salad = Recipe.new('Salad')
 
-    @recipe_ingredients = []
-    @recipe_ingredients << RecipeIngredient.new(pancake, flour)
-    @recipe_ingredients << RecipeIngredient.new(omelette, pepper)
-    @recipe_ingredients << RecipeIngredient.new(salad, carrot)
+    @ri_pf = RecipeIngredient.new(@pancake, @flour)
+    @ri_op = RecipeIngredient.new(@omelette, @pepper)
+    @ri_sc = RecipeIngredient.new(@salad, @carrot)
+
+    @recipe_ingredients = [@ri_pf, @ri_op, @ri_sc]
   end
 
-  describe ".all" do
+  describe '.all' do
 
     it 'should have all instances' do
       expect(RecipeIngredient.all).to eql(@recipe_ingredients)
     end
+  end
+
+  describe 'ingredient' do
+    it 'should have ingredient gettr' do
+      expect(@ri_pf.ingredient).to eql(@flour)
+    end
+
+    it 'should not have ingredient setter' do
+      expect{@ri_pf.ingredient=double}.to raise_error(NoMethodError)
+    end
+  end
+
+  describe 'recipe' do
+    it 'should have recipe gettr' do
+      expect(@ri_pf.recipe).to eql(@pancake)
+    end
+
+    it 'should not have recipe setter' do
+      expect{@ri_pf.recipe=double}.to raise_error(NoMethodError)
+    end
+
   end
 end
