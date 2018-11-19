956f41d29cc4af7ad2bbc5f7127a1b4723012274
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-19 16:21:05 -0500

    formatting

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -2,9 +2,9 @@ require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Ingredient do
-  let(:flour) {Ingredient.new('Flour')}
-  let(:pepper) {Ingredient.new('Pepper')}
-  let(:carrot) {Ingredient.new('Carrot')}
+  let(:flour) { Ingredient.new('Flour') }
+  let(:pepper) { Ingredient.new('Pepper') }
+  let(:carrot) { Ingredient.new('Carrot') }
   let(:ingredients) { [flour, pepper, carrot] }
 
   context '.all' do
