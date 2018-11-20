302bb9cfb2fb21809367a2215aa9c6d6d0f8668d
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-19 19:22:04 -0500

    adding ingredient spec

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe Ingredient do
   let(:flour) { Ingredient.new('Flour') }
   let(:pepper) { Ingredient.new('Pepper') }
   let(:carrot) { Ingredient.new('Carrot') }
+
   let(:ingredients) { [flour, pepper, carrot] }
 
   context '.all' do
     it 'should have all instances' do
       expect(Ingredient.all).to eql(ingredients)
+    end
+  end
+
+  context '.most_common_allergen' do
+    before do
+      shinji = User.new('Shinji')
+      asuka = User.new('Asuka')
+      rei = User.new('Rei')
+
+      Allergen.new(shinji, flour)
+      Allergen.new(rei, flour)
+      Allergen.new(asuka, pepper)
+    end
+
+    it 'should be flour' do
+      expect(Ingredient.most_common_allergen).to eql(flour)
     end
   end
 
