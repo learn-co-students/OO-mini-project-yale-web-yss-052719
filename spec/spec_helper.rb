8a3ffc03384405b56864d63f27dfecdf4576696a
Emily Wilder <emilywilder.earth@gmail.com>
2018-11-26 16:02:25 -0500

    move configuration_spec into spec_helper having them separate was causing the contents of configuration_spec to be loaded twice, resulting in assignment warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,4 +99,18 @@ RSpec.configure do |config|
 =end
 end
 
-require "configuration_spec"
+Dir["./app/models/*.rb"].each { |file| require file }
+require 'pry'
+
+CLASS_LIST = [Allergen, Ingredient, Recipe, RecipeCard, RecipeIngredient, User].freeze
+
+def reset_class_vars(_class)
+  _class.all.clear
+end
+
+RSpec.configure do |config|
+  config.after(:each) do
+    CLASS_LIST.each { |c| reset_class_vars(c) }
+  end
+end
+#
